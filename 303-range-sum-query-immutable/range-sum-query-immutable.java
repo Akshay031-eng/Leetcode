a8b6b6006aee1aa00ccbc863b3b709d7c4d2031a
Akshay031-eng <pratapakshay031@gmail.com>
2026-02-16 21:00:33 +0530

    Time: 9 ms (26.54%) | Memory: 48 MB (25.28%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,13 +1,12 @@
 class NumArray {
 
-    int[] prefix;   // Make it class variable
-
+    int[] prefix;   
     public NumArray(int[] nums) {
         prefix = new int[nums.length];
 
         prefix[0] = nums[0];
 
-        for (int i = 1; i < nums.length; i++) {   // Start from 1
+        for (int i = 1; i < nums.length; i++) {   
             prefix[i] = prefix[i - 1] + nums[i];
         }
     }
