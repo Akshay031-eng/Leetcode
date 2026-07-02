408c60ad7133f11da17475500babfe5bdf2ecf82
Akshay031-eng <pratapakshay031@gmail.com>
2026-07-02 12:07:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (84.94%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,5 +1,6 @@
 class Solution {
     public int fib(int n) {
+        // memorization technique
     //     int dp[]= new int[n+1];
     //     // fill dp with -1;
     //     Arrays.fill(dp,-1);
@@ -18,15 +19,29 @@ class Solution {
     //      int second = helper(n-2, dp);
     //      dp[n]= first + second;
     //      return dp[n];
+    // *********************tabular method
+    // if(n<=1){
+    //     return n;
+    // }
+    // int dp[]= new int[n+1];
+    // dp[0]=0;
+    // dp[1]=1;
+    // for(int i= 2; i<=n ;i++){
+    //     dp[i]= dp[i-1]+ dp[i-2];
+    // }
+    // return dp[n];
+    // ********************************edge casee
     if(n<=1){
         return n;
     }
-    int dp[]= new int[n+1];
-    dp[0]=0;
-    dp[1]=1;
-    for(int i= 2; i<=n ;i++){
-        dp[i]= dp[i-1]+ dp[i-2];
-    }
-    return dp[n];
+    int first = 0;
+    int second = 1;
+     int third = 0;
+     for(int i = 2; i<=n ;i++){
+        third = first +second;
+        first = second;
+        second = third;
+     }
+     return third;
     }
 }
